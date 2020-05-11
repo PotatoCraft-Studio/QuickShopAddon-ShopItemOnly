fb36299aba98eb11418af0c133927167c9193164
Ghost_chu <2908803755@qq.com>
2020-05-11 14:08:31 +0800

    Bug fix

--- src/main/java/cc/bukkit/quickshop/addon/shopitemonly/ShopItemOnly.java
+++ src/main/java/cc/bukkit/quickshop/addon/shopitemonly/ShopItemOnly.java
@@ -1,18 +1,14 @@
 package cc.bukkit.quickshop.addon.shopitemonly;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.block.Container;
-import org.bukkit.event.Event;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.ClickType;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.maxgamer.quickshop.QuickShop;
@@ -22,6 +18,7 @@ import org.maxgamer.quickshop.shop.Shop;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 public final class ShopItemOnly extends JavaPlugin implements Listener {
     private String message;
@@ -29,6 +26,7 @@ public final class ShopItemOnly extends JavaPlugin implements Listener {
     public void onEnable() {
         // Plugin startup logic
         saveDefaultConfig();
+        parseColours(getConfig());
         this.message = getConfig().getString("messages.item-dropped");
 
         Bukkit.getPluginManager().registerEvents(this,this);
@@ -71,6 +69,26 @@ public final class ShopItemOnly extends JavaPlugin implements Listener {
             return;
         }
         event.setCancelled(true);
+    }
+
+    public static void parseColours(FileConfiguration config) {
+        Set<String> keys = config.getKeys(true);
+        for (String key : keys) {
+            String filtered = config.getString(key);
+            if (filtered == null) {
+                continue;
+            }
+            if (filtered.startsWith("MemorySection")) {
+                continue;
+            }
+            filtered = parseColours(filtered);
+            config.set(key, filtered);
+        }
+    }
+
+    public static String parseColours(String text) {
+        text = ChatColor.translateAlternateColorCodes('&', text);
+        return text;
     }
 
 }
